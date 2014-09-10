523616b7b855a3c56092729d7e96a21afdba3061
Tim Levett <levett@wisc.edu>
2014-09-10 10:37:49 -0500

    space fix

--- js/angular-page.js
+++ js/angular-page.js
@@ -1,9 +1,9 @@
 (function() {
  var app = angular.module('portal', [
     'ngRoute',
- 	'portal.controllers',
- 	'portal.directives',
- 	'portal.notificationControllers'
+    'portal.controllers',
+    'portal.directives',
+    'portal.notificationControllers'
  ]);
  app.config(['$routeProvider',function($routeProvider) {
 	 $routeProvider.
