3ebca0b22405196b066bc3e0303968846f22d4d3
Andrew Petro <apetro@wisc.edu>
2014-09-23 16:09:16 -0500

    MUMUP-1125 hide notifications functionality.

--- js/angular-page.js
+++ js/angular-page.js
@@ -5,14 +5,14 @@
     'portal.misc.directives',
     'portal.main.controllers',
     'portal.main.directives',
-    'portal.notification.controller',
+ /* 'portal.notification.controller', */
     'portal.marketplace.controller',
     'portal.marketplace.service'
      ]);
  app.config(['$routeProvider',function($routeProvider, $locationProvider) {
 	 $routeProvider.
       when('/marketplace', {templateUrl: 'partials/marketplace.html'}).
-      when('/notifications', {templateUrl: 'partials/notifications-full.html'}).
+   /* when('/notifications', {templateUrl: 'partials/notifications-full.html'}). */
       when('/marketplace/:fname', {templateUrl: 'partials/marketplace-details.html', controller:'MarketplaceDetailsController'}).
       otherwise({templateUrl: 'partials/main.html'});
       }
