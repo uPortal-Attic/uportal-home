f8141976d590ae76bbdfebf9bc167c6a690feca5
Tim Levett <levett@wisc.edu>
2014-12-18 14:00:13 -0600

    MUMUP-1432 : search box now has focus card

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -97,6 +97,22 @@
                 )
             }
         }
-    })
+    });
+    
+    app.directive('focusMe', function($timeout) {
+        return {
+          link: function(scope, element, attrs) {
+            scope.$watch(attrs.focusMe, function(value) {
+              if(value === true) { 
+                console.log('value=',value);
+                $timeout(function() {
+                  element[0].focus();
+                  scope[attrs.focusMe] = false;
+                });
+              }
+            });
+          }
+        };
+      });
 
 })();
