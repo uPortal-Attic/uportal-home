1792f24d552f11fd8d50a65fc9ecf89dc0c5d998
Tim Levett <levett@wisc.edu>
2014-12-19 12:31:41 -0600

    Add in changing background image of main page header

--- src/main/webapp/js/main/main-controllers.js
+++ src/main/webapp/js/main/main-controllers.js
@@ -6,7 +6,7 @@
   app.controller('MainController', [ '$sessionStorage', '$localStorage', '$rootScope', '$scope', 'mainService', 'miscService', function($sessionStorage, $localStorage, $rootScope, $scope, mainService, miscService) {
     miscService.pushPageview();
     $scope.layout = [];
-    $scope.$storage = $localStorage.$default( {showSidebar: true, sidebarQuicklinks: false} );
+    $scope.$storage = $localStorage.$default( {showSidebar: true, sidebarQuicklinks: false, homeImg : "img/square.jpg"} );
     
 
     mainService.getLayout().then(function(data){
