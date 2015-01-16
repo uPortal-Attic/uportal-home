fca61e176e604b7652023789355fd4aa98b2957f
Andrew Petro <apetro@wisc.edu>
2015-01-15 22:01:34 -0600

    Add commented-out example of MacOS notification and sound on build.

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ popd
 pushd angularjs-portal-home
 mvn -Djava.awt.headless=true tomcat7:redeploy
 popd
+
+# On MacOS this will display a notification.
+# osascript -e 'display notification "angularJSportal build.sh finished" with title "Angular portal deployed" sound name "Hero"'
+# See http://apple.stackexchange.com/questions/57412/
