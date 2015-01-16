141ae9d7d66b90c0762dd6900531316ea53770e3
Andrew Petro <apetro@wisc.edu>
2015-01-15 21:18:33 -0600

    Add MarketplaceController dependency on mainService .

--- angularjs-portal-home/src/main/webapp/js/marketplace/marketplace-controller.js
+++ angularjs-portal-home/src/main/webapp/js/marketplace/marketplace-controller.js
@@ -3,7 +3,7 @@
 (function() {
   var app = angular.module('portal.marketplace.controller', []);
 
-  app.controller('MarketplaceController', [ '$sessionStorage', '$modal', '$timeout', '$rootScope',  '$window', '$http', '$scope','$location','$routeParams','marketplaceService','miscService', function($sessionStorage,$modal,$timeout, $rootScope, $window, $http, $scope, $location, $routeParams, marketplaceService, miscService) {
+  app.controller('MarketplaceController', [ '$sessionStorage', '$modal', '$timeout', '$rootScope',  '$window', '$http', '$scope','$location','$routeParams','marketplaceService','miscService', 'mainService', function($sessionStorage,$modal,$timeout, $rootScope, $window, $http, $scope, $location, $routeParams, marketplaceService, miscService, mainService) {
 
     $scope.$storage = $sessionStorage;
     //init variables
