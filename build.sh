d23968fc3ef38190b22d7a03f1ec982b2780ec06
Jared Hanstra <jared.hanstra@wisc.edu>
2015-01-20 13:59:39 -0600

    move build.sh change to separate pr

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ mvn -Djava.awt.headless=true clean package
 pushd angularjs-portal-frame
 mvn -Djava.awt.headless=true tomcat7:redeploy
 popd
-mvn install
 pushd angularjs-portal-home
 mvn -Djava.awt.headless=true tomcat7:redeploy
 popd
