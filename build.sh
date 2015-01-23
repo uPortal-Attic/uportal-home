5284301c7ef20aa5c1a61f1c952cb0c9da407ee8
Tim Levett <levett@wisc.edu>
2015-01-23 08:33:27 -0600

    add optional parameters to build and add in mac/linux notifications

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+if [ "$#" = "0" ]; then
+ mvn -Djava.awt.headless=true clean package
+else 
+ mvn -Djava.awt.headless=true $@
+fi
 
-mvn -Djava.awt.headless=true clean package
 pushd angularjs-portal-frame
 mvn -Djava.awt.headless=true tomcat7:redeploy
 popd
 pushd angularjs-portal-home
 mvn -Djava.awt.headless=true tomcat7:redeploy
 popd
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+ notify-send "build complete for angular" 
+elif [[ "$unamestr" == 'Darwin' ]]; then
+ osascript -e 'display notification "angularJSportal build.sh finished" with title "Angular portal deployed" sound name "Hero"'
+fi
+
