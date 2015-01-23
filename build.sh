bde04ba7539c8cfce545fd70ef55bd109d295611
Jared Hanstra <jared.hanstra@wisc.edu>
2015-01-23 13:23:07 -0600

    add mvn install to build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ mvn -Djava.awt.headless=true clean package
 pushd angularjs-portal-frame
 mvn -Djava.awt.headless=true tomcat7:redeploy
 popd
+mvn install
 pushd angularjs-portal-home
 mvn -Djava.awt.headless=true tomcat7:redeploy
 popd
