f3c92e16b787d8f433a048960a454263bf18062e
Tim Levett <levett@wisc.edu>
2015-05-06 17:04:07 -0500

    Create a subreddit feed example widget

--- angularjs-portal-home/src/main/java/edu/wisc/my/home/util/KeyUtils.java
+++ angularjs-portal-home/src/main/java/edu/wisc/my/home/util/KeyUtils.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.env.Environment;
 
 public class KeyUtils {
@@ -12,8 +13,10 @@ public class KeyUtils {
   public static Map<String, String> getHeaders(Environment env, HttpServletRequest request, String key) {
     HashMap <String, String> map = new HashMap<String, String>();
     String attributes = env.getProperty(key + ".attributes");
-    for(String attribute : attributes.split(",")) {
-      map.put(attribute, request.getHeader(attribute));
+    if(StringUtils.isNotBlank(attributes)) {
+      for(String attribute : attributes.split(",")) {
+        map.put(attribute, request.getHeader(attribute));
+      }
     }
     return map;
   }
