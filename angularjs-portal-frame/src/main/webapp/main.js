b454373c6e4d19b172c61417b981a71a88a0e44a
Paul Erickson <paul.erickson@wisc.edu>
2015-05-29 16:25:26 -0500

    Configure path to uw-ui-toolkit.js

--- angularjs-portal-frame/src/main/webapp/main.js
+++ angularjs-portal-frame/src/main/webapp/main.js
@@ -6,25 +6,27 @@ require.config({
     ],
 
     paths: {
-        'angular'          : "bower_components/angular/angular",
-        'app-config'       : "js/app-config",
-        'jquery'           : "bower_components/jquery/dist/jquery.min",
-        'jquery-ui'        : "bower_components/jquery-ui/jquery-ui.min",
-        'ngRoute'          : "bower_components/angular-route/angular-route.min",
-        'ngSanitize'       : "bower_components/angular-sanitize/angular-sanitize.min",
-        'ngStorage'        : "bower_components/ngstorage/ngStorage.min",
-        'sortable'         : "js/sortable",
-        'ui-bootstrap'     : "js/ui-bootstrap",
-        'ui-gravatar'      : "bower_components/angular-gravatar/build/angular-gravatar"
+        'angular'       : "bower_components/angular/angular",
+        'app-config'    : "js/app-config",
+        'jquery'        : "bower_components/jquery/dist/jquery.min",
+        'jquery-ui'     : "bower_components/jquery-ui/jquery-ui.min",
+        'ngRoute'       : "bower_components/angular-route/angular-route.min",
+        'ngSanitize'    : "bower_components/angular-sanitize/angular-sanitize.min",
+        'ngStorage'     : "bower_components/ngstorage/ngStorage.min",
+        'sortable'      : "js/sortable",
+        'ui-bootstrap'  : "js/ui-bootstrap",
+        'ui-gravatar'   : "bower_components/angular-gravatar/build/angular-gravatar",
+        'uw-ui-toolkit' : "bower_components/uw-ui-toolkit/dist/js/uw-ui-toolkit.min"
     },
 
     shim: {
-        'angular'     : { deps: ['jquery'], exports: 'angular' },
-        'ngRoute'     : { deps: ['angular'] },
-        'ngSanitize'  : { deps: ['angular'] },
-        'ngStorage'   : { deps: ['angular'] },
-        'ui-bootstrap': { deps: ['angular'] },
-        'ui-gravatar' : { deps: ['angular'] }
+        'angular'       : { deps: ['jquery'], exports: 'angular' },
+        'ngRoute'       : { deps: ['angular'] },
+        'ngSanitize'    : { deps: ['angular'] },
+        'ngStorage'     : { deps: ['angular'] },
+        'ui-bootstrap'  : { deps: ['angular'] },
+        'ui-gravatar'   : { deps: ['angular'] },
+        'uw-ui-toolkit' : { deps: ['jquery'] }
     }
 
 });
