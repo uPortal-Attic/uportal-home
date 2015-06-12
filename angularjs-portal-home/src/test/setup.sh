95e8a54328e33d8f967cb8fef7e8ba8c5480b49e
Tim Levett <levett@wisc.edu>
2015-06-12 10:25:41 -0500

    added unit tests to home

--- angularjs-portal-home/src/test/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-npm install karma --save-dev  
-npm install karma-jasmine --save-dev
-npm install karma-htmlfile-reporter --save-dev
-npm install jasmine-core --save-dev
-npm install grunt --save-dev
-npm install grunt-karma --save-dev
-npm install karma-phantomjs-launcher --save-dev
-
