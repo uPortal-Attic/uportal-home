598e52930f19d4ddeb3791117edd96c662a84cdb
Andrew Petro <apetro@wisc.edu>
2015-10-15 11:10:32 -0500

    MUMUP-2163 : Prompt to search for events when no search results.

--- angularjs-portal-home/src/main/webapp/my-app/marketplace/controllers.js
+++ angularjs-portal-home/src/main/webapp/my-app/marketplace/controllers.js
@@ -145,6 +145,7 @@ define(['angular', 'jquery'], function(angular, $) {
             $scope.webSearchDomain = MISC_URLS.webSearchDomain;
             $scope.directorySearchUrl = MISC_URLS.directorySearchURL;
             $scope.kbSearchUrl = MISC_URLS.kbSearchURL;
+            $scope.eventsSearchUrl = MISC_URLS.eventsSearchURL;
 
             $scope.feedbackUrl = MISC_URLS.feedbackURL;
             $scope.helpdeskUrl = MISC_URLS.helpdeskURL;
