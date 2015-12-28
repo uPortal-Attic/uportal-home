a64e121db7575ecfcdd9ef51eecc62dc1848456e
Tim Levett <levett@wisc.edu>
2015-12-28 14:28:09 -0600

    configure max & exclusive modes to use new add to home

--- build.sh
+++ build.sh
@@ -3,25 +3,22 @@
 if [ "$1" = "master" ]; then
   cp angularjs-portal-home/src/main/webapp/js/app-config.js angularjs-portal-home/src/main/webapp/js/app-config.js.bak
   cp angularjs-portal-home/src/main/webapp/js/master-app-config.js angularjs-portal-home/src/main/webapp/js/app-config.js
-  
+
   mvn -Djava.awt.headless=true clean install
 else
 
   if [ "$#" = "0" ]; then
    mvn -Djava.awt.headless=true clean install
-  else 
+  else
    mvn -Djava.awt.headless=true $@
   fi
 
 fi
-  
-  pushd angularjs-portal-frame
-  mvn -Djava.awt.headless=true tomcat7:redeploy
-  popd
+
   pushd angularjs-portal-home
   mvn -Djava.awt.headless=true tomcat7:redeploy
   popd
-  
+
 if [ "$1" = "master" ]; then
   mv angularjs-portal-home/src/main/webapp/js/app-config.js.bak angularjs-portal-home/src/main/webapp/js/app-config.js
 fi
@@ -29,8 +26,7 @@ fi
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
- notify-send "build complete for angular" 
+ notify-send "build complete for angular"
 elif [[ "$unamestr" == 'Darwin' ]]; then
  osascript -e 'display notification "angularJSportal build.sh finished" with title "Angular portal deployed" sound name "Hero"'
 fi
-
