3d7b37d64ffd06224223bb28cd8d4e799ef12c8f
Tim Levett <levett@wisc.edu>
2016-01-25 12:19:53 -0600

    quotes around things that need quotes

--- angularjs-portal-home/src/main/webapp/my-app/search/services.js
+++ angularjs-portal-home/src/main/webapp/my-app/search/services.js
@@ -4,7 +4,7 @@ define(['angular', 'jquery'], function(angular, $) {
 
     var app = angular.module('my-app.search.services', []);
     
-    app.factory('googleCustomSearchService', ['$http', 'miscService', SERVICE_LOC, function($http, miscService, SERVICE_LOC){
+    app.factory('googleCustomSearchService', ['$http', 'miscService', 'SERVICE_LOC', function($http, miscService, SERVICE_LOC){
       
       function googleSearch(term) {
         return $http.get(SERVICE_LOC.googleSearchURL + term).then(
