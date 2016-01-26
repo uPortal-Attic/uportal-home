b77f4a8c371499d6820ea8eabdfee73a75e92e5f
Timothy Vertein <vertein@wisc.edu>
2016-01-26 11:08:06 -0600

    MUMUP-2198 MUMUP-2199 Add more media types to RP

--- angularjs-portal-home/src/main/java/edu/wisc/my/home/api/RestProxyApplicationConfiguration.java
+++ angularjs-portal-home/src/main/java/edu/wisc/my/home/api/RestProxyApplicationConfiguration.java
@@ -1,10 +1,18 @@
 package edu.wisc.my.home.api;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import edu.wisc.my.restproxy.config.RestProxyConfiguration;
@@ -24,6 +32,26 @@ public class RestProxyApplicationConfiguration {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    /**
+     * This bean adds in unsupported return types.   Necessary because even though
+     * rest proxy accepts all types, there are still a few that are unsupported
+     *  
+     * @return a {@link RestTemplate}
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        RestTemplate restTemplate=new RestTemplate();
+        List<HttpMessageConverter<?>> mc=restTemplate.getMessageConverters();
+        MappingJackson2HttpMessageConverter json=new MappingJackson2HttpMessageConverter();
+        List<MediaType> supportedMediaTypes=new ArrayList<MediaType>();
+        supportedMediaTypes.add(new MediaType("text","javascript",Charset.forName("utf-8")));
+        supportedMediaTypes.add(new MediaType("application", "javascript", Charset.forName("UTF-8")));
+        json.setSupportedMediaTypes(supportedMediaTypes);
+        mc.add(json);
+        restTemplate.setMessageConverters(mc);
+        return restTemplate;
     }
 
 }
