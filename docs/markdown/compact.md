1ee10c58a55c114945db413aac3cc55e30aa2c9e
Tim Levett <tim.levett@gmail.com>
2016-03-17 11:16:21 -0500

    add in markdown file and dockerfile for building the dockerfile

---
+++ docs/markdown/compact.md
@@ -0,0 +1,1 @@
+#### TODO : Add documentation about compact mode
---
+++ docs/override.js
@@ -0,0 +1,24 @@
+define(['angular'], function(angular) {
+
+  /*Keep in sync with docs/mardown/configuration.md*/
+
+    var config = angular.module('override', []);
+    config
+        //see configuration.md for howto
+        .constant('OVERRIDE', {
+          'APP_FLAGS' : {
+            'showSearch' : false
+          },
+          'NAMES' : {
+            'title' : 'AngularJS-Portal Docs',
+            'fname' : 'ajsp-docs'
+          },
+          'MISC_URLS' : {
+            'rootURL' : '#/',
+            'loginURL' : 'https://github.com/UW-Madison-DoIT/angularjs-portal'
+          }
+        })
+
+    return config;
+
+});
