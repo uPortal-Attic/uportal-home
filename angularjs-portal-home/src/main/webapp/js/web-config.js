5b40edcdc719bf74cdbaccd21d6c351eb1cefd74
Doug Reed <doug.reed@wisc.edu>
2016-05-17 14:02:58 -0500

    MUMUP-2548 Fix Web search for Madison

--- angularjs-portal-home/src/main/webapp/js/web-config.js
+++ angularjs-portal-home/src/main/webapp/js/web-config.js
@@ -15,10 +15,10 @@ define(['angular'], function(angular) {
       {
         "group" : "UW System-River Falls",
         "directorySearchURL" : "/web/api/uwrfdirectory",
-        "googleSearchURL" : "/web/api/uwrfsearch?key=AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY&rsz=10&num=10&hl=en&prettyPrint=false&source=gcsc&gss=.com&sig=432dd570d1a386253361f581254f9ca1&cx=004071655910512460416:8kmve-tofw8&googlehost=www.google.com&nocache=1456777578251&",
+        "googleSearchURL" : "/web/api/uwrfsearch?key=AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY&rsz=10&num=10&hl=en&prettyPrint=false&source=gcsc&gss=.com&sig=432dd570d1a386253361f581254f9ca1&cx=004071655910512460416:8kmve-tofw8&googlehost=www.google.com&nocache=1456777578251&q=",
         "webSearchURL" : "https://www.uwrf.edu/AboutUs/SearchResults.cfm?q=",
         "domainResultsLabel" : "UWRF.edu",
-        'helpdeskURL' : 'https://kb.wisc.edu/helpdesk/'
+        'helpdeskURL' : "https://technology.uwrf.edu/TDClient/Home/"
       }
     ])
     return config;
