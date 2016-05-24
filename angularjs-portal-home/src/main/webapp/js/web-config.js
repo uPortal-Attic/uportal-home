74d28746c945b9e972cb21a763b629c42f749be0
Timothy Vertein <vertein@wisc.edu>
2016-05-24 11:50:48 -0500

    MUMUP-2548 Fixes wisc.edu google search

--- angularjs-portal-home/src/main/webapp/js/web-config.js
+++ angularjs-portal-home/src/main/webapp/js/web-config.js
@@ -5,7 +5,7 @@ define(['angular'], function(angular) {
       {
         "group" : "UW-Madison",
         "directorySearchURL" : "/web/api/wiscdirectory",
-        "googleSearchURL" : "/web/api/wiscedusearch?v=1.0&rsz=10&start=0&cx=001601028090761970182:2g0iwqsnk2m",
+        "googleSearchURL" : "/web/api/wiscedusearch?key=AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY&rsz=10&num=10&sig=b16e3ffbc96e4526fdc0cc8394bf713a&cx=001601028090761970182:uu2tbvfp4za&alt=json",
         "webSearchURL" : "http://www.wisc.edu/search/?q=",
         "domainResultsLabel" : "Wisc.edu",
         'kbSearchURL' : 'https://kb.wisc.edu/search.php?q=',
