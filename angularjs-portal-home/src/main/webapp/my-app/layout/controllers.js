a812fda45ed5fbf31db12857f7b201178febb731
Tim Levett <tim.levett@wisc.edu>
2016-06-21 12:05:32 -0500

    single page with just a widget

--- angularjs-portal-home/src/main/webapp/my-app/layout/controllers.js
+++ angularjs-portal-home/src/main/webapp/my-app/layout/controllers.js
@@ -111,8 +111,90 @@ define(['angular', 'jquery'], function(angular, $) {
 
         }]);
 
+   app.controller('BaseWidgetFunctionsController', [
+     '$scope',
+     '$location',
+     '$sessionStorage',
+     '$localStorage',
+     'sharedPortletService',
+     'layoutService',
+     'childController',
+     function($scope,
+              $location,
+              $sessionStorage,
+              $localStorage,
+              sharedPortletService,
+              layoutService,
+              childController) {
+       childController.portletType = function portletType(portlet) {
+           if (portlet.widgetType) {
+               if('option-link' === portlet.widgetType) {
+                   return "OPTION_LINK";
+               } else if('weather' === portlet.widgetType) {
+                   return "WEATHER";
+               } else if('generic' === portlet.widgetType) {
+                   return "GENERIC";
+               } else if('rss' === portlet.widgetType) {
+                   return "RSS";
+               } else if('list-of-links' === portlet.widgetType) {
+                   return "LOL";
+               } else if ('search-with-links' === portlet.widgetType) {
+                   return "SWL";
+               } else {
+                   return "WIDGET";
+               }
+
+           }else if(portlet.pithyStaticContent != null) {
+               return "PITHY";
+           } else if (portlet.staticContent != null
+               && portlet.altMaxUrl == false) {
+               return "SIMPLE";
+           } else {
+               return "NORMAL";
+           }
+       };
+
+       childController.renderURL = function renderURL(portlet) {
+         if(portlet.altMaxUrl == false && (portlet.renderOnWeb || $localStorage.webPortletRender)) {
+           return 'exclusive/' + portlet.fname;
+         } else {
+           return portlet.url;
+         }
+       }
+
+       childController.maxStaticPortlet = function gotoMaxStaticPortlet(portlet) {
+           sharedPortletService.setProperty(portlet);
+           $location.path('/static/'+portlet.fname);
+       };
+
+       childController.directToPortlet = function directToPortlet(url) {
+           $location.path(url);
+       };
+       childController.removePortlet = function removePortletFunction(nodeId, title) {
+           layoutService.removeFromHome(nodeId, title).success(function(){
+               $scope.$apply(function(request, text){
+                   var result = $.grep($scope.layout, function(e) { return e.nodeId === nodeId});
+                   var index = $.inArray(result[0], $scope.layout);
+                   //remove
+                   $scope.layout.splice(index,1);
+                   if($sessionStorage.marketplace != null) {
+                       var marketplaceEntries = $.grep($sessionStorage.marketplace, function(e) { return e.fname === result[0].fname});
+                       if(marketplaceEntries.length > 0) {
+                           marketplaceEntries[0].hasInLayout = false;
+                       }
+                   }
+               });
+           }).error(
+           function(request, text, error){
+               alert('Issue deleting ' + title + ' from your list of favorites, try again later.');
+           });
+       };
+     }
+   ]);
+
 
     app.controller('WidgetController', [
+        '$controller',
         '$location',
         '$localStorage',
         '$sessionStorage',
@@ -121,7 +203,8 @@ define(['angular', 'jquery'], function(angular, $) {
         'layoutService',
         'miscService',
         'sharedPortletService',
-        function($location,
+        function($controller,
+                 $location,
                  $localStorage,
                  $sessionStorage,
                  $scope,
@@ -129,96 +212,36 @@ define(['angular', 'jquery'], function(angular, $) {
                  layoutService,
                  miscService,
                  sharedPortletService) {
-            if(typeof $rootScope.layout === 'undefined' || $rootScope.layout == null) {
+        var base = $controller('BaseWidgetFunctionsController', { $scope : $scope, childController : this });
 
-                $rootScope.layout = [];
+        function init() {
+          if(typeof $rootScope.layout === 'undefined' || $rootScope.layout == null) {
+            $rootScope.layout = [];
+            layoutService.getLayout().then(function(data){
+              $rootScope.layout = data.layout;
+              });
+           }
+         }
 
-                layoutService.getLayout().then(function(data){
-                    $rootScope.layout = data.layout;
-                });
-            }
+          $scope.sortableOptions = {
+              delay:250,
+              cursorAt : {top: 30, left: 30},
+              stop: function(e, ui) {
+                  if(ui.item.sortable.dropindex != ui.item.sortable.index) {
 
-            this.portletType = function portletType(portlet) {
-                if (portlet.widgetType) {
-                    if('option-link' === portlet.widgetType) {
-                        return "OPTION_LINK";
-                    } else if('weather' === portlet.widgetType) {
-                        return "WEATHER";
-                    } else if('generic' === portlet.widgetType) {
-                        return "GENERIC";
-                    } else if('rss' === portlet.widgetType) {
-                        return "RSS";
-                    } else if('list-of-links' === portlet.widgetType) {
-                        return "LOL";
-                    } else if ('search-with-links' === portlet.widgetType) {
-                        return "SWL";
-                    } else {
-                        return "WIDGET";
-                    }
-
-                }else if(portlet.pithyStaticContent != null) {
-                    return "PITHY";
-                } else if (portlet.staticContent != null
-                    && portlet.altMaxUrl == false) {
-                    return "SIMPLE";
-                } else {
-                    return "NORMAL";
-                }
-            };
-
-            this.renderURL = function renderURL(portlet) {
-              if(portlet.altMaxUrl == false && (portlet.renderOnWeb || $localStorage.webPortletRender)) {
-                return 'exclusive/' + portlet.fname;
-              } else {
-                return portlet.url;
-              }
-            }
-
-            this.maxStaticPortlet = function gotoMaxStaticPortlet(portlet) {
-                sharedPortletService.setProperty(portlet);
-                $location.path('/static/'+portlet.fname);
-            };
-
-            this.directToPortlet = function directToPortlet(url) {
-                $location.path(url);
-            };
-            this.removePortlet = function removePortletFunction(nodeId, title) {
-                layoutService.removeFromHome(nodeId, title).success(function(){
-                    $scope.$apply(function(request, text){
-                        var result = $.grep($scope.layout, function(e) { return e.nodeId === nodeId});
-                        var index = $.inArray(result[0], $scope.layout);
-                        //remove
-                        $scope.layout.splice(index,1);
-                        if($sessionStorage.marketplace != null) {
-                            var marketplaceEntries = $.grep($sessionStorage.marketplace, function(e) { return e.fname === result[0].fname});
-                            if(marketplaceEntries.length > 0) {
-                                marketplaceEntries[0].hasInLayout = false;
-                            }
-                        }
-                    });
-                }).error(
-                function(request, text, error){
-                    alert('Issue deleting ' + title + ' from your list of favorites, try again later.');
-                });
-            };
-
-            $scope.sortableOptions = {
-                delay:250,
-                cursorAt : {top: 30, left: 30},
-                stop: function(e, ui) {
-                    if(ui.item.sortable.dropindex != ui.item.sortable.index) {
-
-                        var node = $scope.layout[ui.item.sortable.dropindex];
+                      var node = $scope.layout[ui.item.sortable.dropindex];
+                      if(console) {
                         console.log("Change happened, logging move of " + node.fname + " from " + ui.item.sortable.index + " to " + ui.item.sortable.dropindex);
-                        //index, length, movingNodeId, previousNodeId, nextNodeId
-                        var prevNodeId = ui.item.sortable.dropindex != 0 ? $scope.layout[ui.item.sortable.dropindex - 1].nodeId : "";
-                        var nextNodeId = ui.item.sortable.dropindex != $scope.layout.length - 1 ? $scope.layout[ui.item.sortable.dropindex + 1].nodeId : "";
-                        layoutService.moveStuff(ui.item.sortable.dropindex, $scope.layout.length, node.nodeId, prevNodeId, nextNodeId);
+                      }
+                      //index, length, movingNodeId, previousNodeId, nextNodeId
+                      var prevNodeId = ui.item.sortable.dropindex != 0 ? $scope.layout[ui.item.sortable.dropindex - 1].nodeId : "";
+                      var nextNodeId = ui.item.sortable.dropindex != $scope.layout.length - 1 ? $scope.layout[ui.item.sortable.dropindex + 1].nodeId : "";
+                      layoutService.moveStuff(ui.item.sortable.dropindex, $scope.layout.length, node.nodeId, prevNodeId, nextNodeId);
+                  }
+              }
+          };
 
-                    }
-                }
-            };
-
+          init();
         }]);
 
     app.controller('NewStuffController', ['$scope', 'layoutService', function ($scope, layoutService){
