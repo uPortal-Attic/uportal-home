421658255217868619cd7e1a52a938ba2a6ca598
Tim Levett <tim.levett@wisc.edu>
2016-07-25 08:35:22 -0500

    #487 : Fix build master script so it works with vanilla uPortal with uPortal context

---
+++ angularjs-portal-home/src/main/webapp/js/master-override.js
@@ -0,0 +1,83 @@
+define(['angular'], function(angular) {
+
+  /*Keep in sync with docs/markdown/configuration.md*/
+
+    var config = angular.module('override', []);
+    config
+        //see configuration.md for howto
+        .constant('OVERRIDE', {
+          'APP_FLAGS' : {
+            'enableToggle': true,
+            'defaultView' : 'expanded',
+            'compact' : true,
+            'expanded' : true,
+            'isWeb' : true,
+            'defaultTheme' : 'group',
+            'debug' : true,
+            'showUserSettingsPage' : true
+          },
+          'FEATURES' : {
+            enabled: true
+          },
+          'SERVICE_LOC' : {
+            'aboutURL' : '/portal/web/session.json',
+            'sessionInfo' : '/uPortal/api/session.json',
+            'sidebarInfo' : '/web/staticFeeds/sidebar.json',
+            'newstuffInfo': '/web/staticFeeds/new-stuff.json',
+            'context'     : '/uPortal/',
+            'base'        : '/uPortal/api/',
+            'layout'      : 'layoutDoc?tab=UW Bucky Home',
+            'layoutTab' : 'UW Bucky Home',
+            'marketplace' : {
+                'base' : 'marketplace/',
+                'entry' : 'entry/',
+                'entries' : 'entries.json'
+            },
+            'notificationsURL' : '/web/staticFeeds/notifications.json',
+            'groupURL' : '/uPortal/api/groups'
+            'kvURL' : null,
+            'loginSilentURL' : '/uPortal/Login?silent=true'
+          },
+          'NAMES' : {
+              'title' : 'uPortal',
+              'ariaLabelTitle' : 'U Portal',
+              'crest' : 'img/uPortal_400.png',
+              'crestalt' : 'U Portal Crest',
+              'sublogo' : '',
+              'guestUserName' : 'guest'
+          },
+          'NOTIFICATION' : {
+              'enabled' : true,
+              'groupFiltering' : true
+          },
+          'MISC_URLS' : {
+            'feedbackURL' : '/uPortal/p/feedback',
+            'whatsNewURL' : '/web/features'
+          },
+          'APP_BETA_FEATURES' : [
+            {
+              "id" : "webPortletRender",
+              "title" : "/web portlet rendering",
+              "description" : "Renders portlets via /web's exclusive page, but not as launched from expanded widgets."
+            },
+            {
+              "id" : "showKeywordsInMarketplace",
+              "title" : "Show Keywords in Marketplace",
+              "description" : "Enable/Disable keywords showing up in marketplace details"
+            },
+            {
+              "id" : "exampleWidgets",
+              "title" : "Example Widgets",
+              "description" : "Show the My Courses, Email, and Calendar example widgets"
+            },
+            {
+              "id" : "showFilterOption",
+              "title" : "Show Filter Option on Home",
+              "description" : "Enables a filter on home to filter ones content down to what want"
+            }
+          ]
+        });
+
+    return config;
+
+});
