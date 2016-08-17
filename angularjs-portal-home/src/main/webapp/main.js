974aeba12b1d2a10c491ce51554e61b1d85ff8ec
Tim Levett <tim.levett@wisc.edu>
2016-08-17 08:14:34 -0500

    make it work exactly the way login works now

--- angularjs-portal-home/src/main/webapp/main.js
+++ angularjs-portal-home/src/main/webapp/main.js
@@ -4,63 +4,65 @@ require(['./config'], function(config) {
     require.config(config);
 
     require(['angular', 'jquery', 'my-app'], function(angular, $) {
+      //var myApp = angular.module("my-app", []);
       //attempting to replicate what they did in
       //https://blog.mariusschulz.com/2014/10/22/asynchronously-bootstrapping-angularjs-applications-with-server-side-data
-      fetchData().then(bootstrapApplication);
+      doLogin().then(bootstrapApplication);
 
-      function fetchData() {
-        var initInjector = angular.injector(["ng"]);
-        var $http = initInjector.get("$http");
-
-        return $http.get("/portal/Login?silent=true&profile=bucky").then(function(response) {
-            myApplication.constant("config", response.data);
-        }, function(errorResponse) {
-            // Handle error case
-            $('#loading-splash').html('<b>An error has occured during loading, please try refreshing the page. If the issue persists please contact the helpdesk.</b>');
-            console.error("Issue logging in.")
-        });
-    }
-
+      //Bootstrap the application like normal now
       function bootstrapApplication(){
         angular.bootstrap(document, ['my-app']);
+      }
+
+      //Checks if the last login is still valid (4 hour timeout)
+      function lastLoginValid($sessionStorage) {
+        var timeLapseBetweenLogins = 14400000;
+        if($sessionStorage.portal && $sessionStorage.portal.lastAccessed) {
+          var now = (new Date()).getTime();
+          if(now - $sessionStorage.portal.lastAccessed <= timeLapseBetweenLogins) {//4 hours
+            return true;
+          }
+        }
+        return false;
+      };
+
+      //Do the login
+      function doLogin() {
+        //init stuff
+        var initInjector = angular.injector(["ng", "ngStorage"]);
+        var $sessionStorage = initInjector.get("$sessionStorage");
+        var $rootScope = initInjector.get('$rootScope');
+
+        //login stuff
+        if(!lastLoginValid($sessionStorage)) {
+          //assume not valid, go get a username and bootstrap the user
+          var $http = initInjector.get("$http");
+          return $http.get("/portal/Login?silent=true&profile=bucky").then(function(response){
+            if("success" === response.data.status) {
+              //store some meta data for caching reason
+              if(!$sessionStorage.portal){
+                $sessionStorage.portal = {};
+              }
+              $sessionStorage.portal.lastAccessed = (new Date).getTime();
+              $sessionStorage.portal.username = response.data.username;
+              if (response.data.username === 'guest') {
+                $rootScope.GuestMode = true;
+              }
+
+            }
+          },
+          function(reason){
+            //something bad happened
+            $('#loading-splash').html('<b>An error has occured during loading, please try refreshing the page. If the issue persists please contact the helpdesk.</b>');
+            console.error("Issue logging in.");
+            return "Issue with login";
+          });
+        } else {
+          //the cache is still valid with a valid session, carry on
+          var $q = initInjector.get('$q');
+          return $q.resolve("Login cache still valid from previous login");
+        }
       }
     });
 
 });
-
-
-/*
-
-var lastLoginValid = function() {
-  var timeLapseBetweenLogins = APP_FLAGS.loginDurationMills || 14400000;
-  if($sessionStorage.portal && $sessionStorage.portal.lastAccessed) {
-    var now = (new Date()).getTime();
-    if(now - $sessionStorage.portal.lastAccessed <= timeLapseBetweenLogins) {//4 hours
-      return true;
-    }
-  }
-  return false;
-};
-
-if(APP_FLAGS.loginOnLoad && !lastLoginValid()) {
-  $http.get(SERVICE_LOC.loginSilentURL).then(function(result){
-    if(APP_FLAGS.debug) {
-      console.log("login returned with " + (result.data ? result.data.status : null));
-    }
-    themeLoading();
-    if("success" === result.data.status) {
-      $sessionStorage.portal.lastAccessed = (new Date).getTime();
-      $sessionStorage.portal.username = result.data.username;
-      if (NAMES.guestUserName && result.data.username === NAMES.guestUserName) {
-        $rootScope.GuestMode = true;
-      }
-
-    }
-  },
-  function(reason){
-    themeLoading(); //still continue with theme loading so they don't get stuck on loading
-  });
-} else {
-  themeLoading();
-}
-*/
