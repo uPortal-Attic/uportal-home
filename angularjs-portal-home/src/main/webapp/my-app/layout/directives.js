abcce54dd9a2a141fab12934bbf6c4ededd404dd
Christian Murphy <cmurphy@unicon.net>
2017-03-29 16:33:57 -0700

    Issue #604: Apply John Papa Angular code style to layout directives

--- angularjs-portal-home/src/main/webapp/my-app/layout/directives.js
+++ angularjs-portal-home/src/main/webapp/my-app/layout/directives.js
@@ -1,44 +1,41 @@
 'use strict';
 
 define(['angular', 'require'], function(angular, require) {
-    var app = angular.module('my-app.layout.directives', []);
+    return angular.module('my-app.layout.directives', [])
 
-    app.directive('portletIcon', function() {
+    .directive('portletIcon', function() {
         return {
             restrict: 'E',
             templateUrl: require.toUrl('./partials/portlet-icon.html'),
         };
-    });
+    })
 
-    app.directive('defaultCard', function() {
+    .directive('defaultCard', function() {
         return {
             restrict: 'E',
             templateUrl: require.toUrl('./partials/default-card.html'),
         };
-    });
+    })
 
-    app.directive('marketplaceLight', function() {
+    .directive('marketplaceLight', function() {
         return{
             restrict: 'E',
             templateUrl: require.toUrl('./partials/marketplace-light.html'),
         };
-    });
+    })
 
-    app.directive('homeHeader', function() {
+    .directive('homeHeader', function() {
         return{
             restrict: 'E',
             templateUrl: require.toUrl('./partials/home-header.html'),
         };
-    });
+    })
 
-    app.directive('homeToggle', function() {
+    .directive('homeToggle', function() {
         return {
             restrict: 'E',
             templateUrl: require.toUrl('./partials/home-toggle.html'),
             controller: 'ToggleController',
         };
     });
-
-    return app;
 });
-
