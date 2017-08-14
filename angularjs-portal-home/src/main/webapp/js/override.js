1b63f8b414402963c5d3ac275548629e42baf9bb
zeke-witter <david.witter@wisc.edu>
2017-08-14 13:23:08 -0500

    Use new app options model

--- angularjs-portal-home/src/main/webapp/js/override.js
+++ angularjs-portal-home/src/main/webapp/js/override.js
@@ -11,6 +11,9 @@ define(['angular'], function(angular) {
             'debug': true,
             'showUserSettingsPage': true,
           },
+          'APP_OPTIONS': {
+            'optionsTemplateURL': 'my-app/layout/partials/app-header-options.html',
+          },
           'FEATURES': {
             enabled: true,
           },
