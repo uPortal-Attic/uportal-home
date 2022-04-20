14aa3b6e496a642866b34d9c870c04104c7e9d98
Andrew Petro <andrew.petro@wisc.edu>
2022-04-20 08:41:00 -0500

    remove support for old layout backend

--- web/src/main/webapp/my-app/marketplace/controllers.js
+++ web/src/main/webapp/my-app/marketplace/controllers.js
@@ -122,43 +122,6 @@ define(['angular', 'jquery', 'require'], function(angular, $, require) {
         };
       }
 
-      if (APP_FLAGS.useOldLayout) {
-       $scope.addToHome = function addToHome(portlet) {
-        console.log("market: portlet", portlet);
-          var fname = portlet.fname;
-          var ret = layoutService.addToHome(portlet);
-          console.log("market: fname", fname);
-          console.log("market: ret", ret);
-          ret.success(function(request, text) {
-            angular.element('.fname-'+fname)
-              .html('<i class="fa fa-check"></i> Added Successfully')
-              .prop('disabled', true)
-              .removeClass('btn-add')
-              .addClass('btn-added');
-            $scope.$apply(function() {
-              var marketplaceEntries = $.grep(
-                $sessionStorage.marketplace,
-                function(e) {
-                  return e.fname === portlet.fname;
-                }
-              );
-              if (marketplaceEntries.length > 0) {
-                marketplaceEntries[0].hasInLayout = true;
-              }
-              $rootScope.layout = null; // reset layout due to modifications
-              $sessionStorage.layout = null;
-            });
-          })
-            .error(function(request, text, error) {
-              angular.element('.fname-'+fname)
-                .parent()
-                .append(
-                  '<span>Issue adding to home, please try again later</span>'
-                );
-            });
-        };
-      }
-
       $scope.searchTermFilter = function(portlet) {
         return marketplaceService.portletMatchesSearchTerm(
           portlet,
